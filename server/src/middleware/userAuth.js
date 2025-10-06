105d9b3f7d71f80524b36ad4a762445ee436d9e9
bev8090 <bev@njit.edu>
2025-10-06 19:57:56 -0400

    Edited cookies for production

--- server/src/middleware/userAuth.js
+++ server/src/middleware/userAuth.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export const userAuth = async (req, res, next) => {
-  const {token} = req.cookies;
+  const { token } = req.cookies;
 
-  if(!token){
+  if (!token) {
     console.log("No token detected.");
-    return res.status(401).json({message: "Please log in to view this page."});
+    return res.status(401).json({ message: "Please log in to view this page." });
   }
 
   try {
@@ -16,14 +16,14 @@ export const userAuth = async (req, res, next) => {
   } catch (error) {
     res.clearCookie("token");
     console.log("Invalid token", error);
-    res.status(401).json({message: "Token not valid. Please log in again."});
+    res.status(402).json({ message: "Token not valid. Please log in again." });
   }
 };
 
 export const adminAuth = async (req, res, next) => {
-  if(!req.user.isAdmin){
+  if (!req.user.isAdmin) {
     console.log("Admin access required");
-    return res.status(403).json({message: "Admin access required."});
+    return res.status(403).json({ message: "Admin access required." });
   }
   next();
 };
--- server/src/routes/usersRoutes.js
+++ server/src/routes/usersRoutes.js
@@ -14,58 +14,58 @@ router.get("/", async (_, res) => {
     res.status(200).json(users);
   } catch (error) {
     console.log(`Error in fetching users: ${error.message}`);
-    res.status(500).json({message: "Internal server error"});
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
 // Find user by id
 router.get("/:id", async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const user = await User.findById(id);
 
-    if(!user){
-      return res.status(404).json({message: "User not found"});
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
     res.json(user);
   } catch (error) {
     console.error("Error in retrieving user", error.message);
-    res.status(500).json({message: "Internal service error"});
+    res.status(500).json({ message: "Internal service error" });
   }
 });
 
 // Create a new user
 router.post("/register", async (req, res) => {
   try {
-    const {username, email, password} = req.body;
+    const { username, email, password } = req.body;
 
-    if(!username || !email || !password){
+    if (!username || !email || !password) {
       console.log("All registration fields required");
-      return res.status(400).json({message: "Please provide all input fields"});
+      return res.status(400).json({ message: "Please provide all input fields" });
     }
 
     try {
-      const existingUser = await User.findOne({email});
-      if(existingUser){
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
         console.log("User already exists. Register failed.");
-        return res.status(400).json({message: "User already exists with same email"});
+        return res.status(400).json({ message: "User already exists with same email" });
       }
     } catch (error) {
       console.log("Error with searching for existing user");
-      return res.status(500).json({message: "Error with searching for existing user"});
+      return res.status(500).json({ message: "Error with searching for existing user" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({username, email, password: hashedPassword});
+    const user = new User({ username, email, password: hashedPassword });
     await user.save();
 
-    const token = jwt.sign({userId: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET, {expiresIn: "1d"});
+    const token = jwt.sign({ userId: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: "1d" });
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true, //process.env.NODE_ENV === "production",
+      sameSite: "none", //process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 1 * 24 * 60 * 60 * 1000
     });
 
@@ -82,48 +82,48 @@ router.post("/register", async (req, res) => {
     } catch (error) {
       console.log("Error with mail transporter", error);
     }
-    
-    res.status(201).json({user: {userId: user._id, username, email, isAdmin: user.isAdmin}, message: "New user successfully created"});
+
+    res.status(201).json({ user: { userId: user._id, username, email, isAdmin: user.isAdmin }, message: "New user successfully created" });
   } catch (error) {
     console.error(`Error in creating user: ${error.message}`);
-    res.status(500).json({message: "Internal server error", error: error.message});
+    res.status(500).json({ message: "Internal server error", error: error.message });
   }
 });
 
 // Login an existing user
 router.post("/login", async (req, res) => {
   try {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
-    if(!email || !password){
-      return res.status(400).json({message: "Please provide all input fields"});
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please provide all input fields" });
     }
 
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
 
-    if(!user){
-      return res.status(400).json({message: "Incorrect email or password."});
+    if (!user) {
+      return res.status(400).json({ message: "Incorrect email or password." });
     }
 
     const isMatch = await user.comparePassword(password); // or bcrypt.compare(password, user.password)
-    
-    if(!isMatch){
-      return res.status(400).json({message: "Incorrect email or password."});
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Incorrect email or password." });
     }
-    
-    const token = jwt.sign({userId: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET, {expiresIn: "1d"});
+
+    const token = jwt.sign({ userId: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: "1d" });
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true, //process.env.NODE_ENV === "production",
+      sameSite: "none", //process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 1 * 24 * 60 * 60 * 1000
     });
 
-    res.status(200).json({user: {userId: user._id, username: user.username, email, isAdmin: user.isAdmin}, message: "Successfully logged in"});
+    res.status(200).json({ user: { userId: user._id, username: user.username, email, isAdmin: user.isAdmin }, message: "Successfully logged in" });
   } catch (error) {
     console.error(`Error in logging in user: ${error.message}`);
-    res.status(500).json({message: "Internal server error", error: error.message});
+    res.status(500).json({ message: "Internal server error", error: error.message });
   }
 });
 
@@ -132,30 +132,30 @@ router.post("/logout", async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
+      secure: true, //process.env.NODE_ENV === "production",
+      sameSite: "none", //process.env.NODE_ENV === "production" ? "none" : "strict",
     });
 
-    res.status(200).json({message: "Successfully logged out"});
+    res.status(200).json({ message: "Successfully logged out" });
   } catch (error) {
-    res.status(500).json({message: "Server error, unable to log out. Please try again later."});
+    res.status(500).json({ message: "Server error, unable to log out. Please try again later." });
   }
 });
 
 // Check if user is logged in
 router.get('/check-auth', async (req, res) => {
-  const {token} = req.cookies;
-  
-  if(!token){
-    return res.status(401).json({loggedIn: false});
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(401).json({ loggedIn: false });
   }
-  
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    res.status(200).json({loggedIn: true, user: decoded})
+    res.status(200).json({ loggedIn: true, user: decoded })
   } catch (error) {
     res.clearCookie("token");
-    res.status(401).json({loggedIn: false});
+    res.status(401).json({ loggedIn: false });
   }
 });
 
@@ -166,7 +166,7 @@ router.get('/profile', userAuth, async (req, res) => {
     res.status(200).json(user);
   } catch (error) {
     console.log("Error with profile endpoint", error);
-    res.status(500).json({ message: 'Server error. Please try again later.', error: error.message});
+    res.status(500).json({ message: 'Server error. Please try again later.', error: error.message });
   }
 });
 
@@ -193,36 +193,36 @@ router.get('/admin', userAuth, adminAuth, async (req, res) => {
 // Update a user by id
 router.put("/:id", async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const user = req.body;
 
-    const updatedUser = await User.findByIdAndUpdate(id, user, {new: true});
-    
-    if(!updatedUser){
-      res.status(404).json({message: "User not found"});
+    const updatedUser = await User.findByIdAndUpdate(id, user, { new: true });
+
+    if (!updatedUser) {
+      res.status(404).json({ message: "User not found" });
     }
 
     res.status(200).json(updatedUser);
   } catch (error) {
     console.error("Error in updating user:", error.message);
-    res.status(500).json({message: "Internal service error"});
+    res.status(500).json({ message: "Internal service error" });
   }
 });
 
 // Delete a user by id
 router.delete("/:id", async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const deletedUser = await User.findByIdAndDelete(id);
 
-    if(!deletedUser){
-      return res.status(404).json({message: "User not found"});
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
     }
 
-    res.status(200).json({message: "User successfully deleted"});
+    res.status(200).json({ message: "User successfully deleted" });
   } catch (error) {
     console.error("Error in deleting user:", error.message);
-    res.status(500).json({message: "Internal service error"});
+    res.status(500).json({ message: "Internal service error" });
   }
 });
 
